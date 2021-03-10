2d4cc040761e95bb987c7b382782fc6b93dca103
zhouya <zhouya@meituan.com>
2021-03-10 20:16:28 +0800

    加两题

--- main.py
+++ main.py
@@ -29,9 +29,19 @@ for i in range(len(tempList)-1,-1,-1):
 tempstr = "hello you hello python are you ok"
 import re
 rex = r'(hello|Use)'
-print re.sub(rex,"Bye",tempstr)
+print(re.sub(rex,"Bye",tempstr))
 
 # list 对象 alist [{“name”:”a”,”age”:20},{“name”:”b”,”age”:30},{“name”:”c”,”age”:25}]按照 age 从大到小排序
 alist = [{"name":"a","age":20},{"name":"b","age":30},{"name":"c","age":25}]
-alist.sort(key='age')
-print alist
+alist.sort(key=lambda x:-x['age'])#sort改变本身，且只对list
+test=sorted(alist,key=lambda x:x['age'],reverse=True)    #sorted对所有对象都可以，不会改变本省,reverse表示升序还是降序，默认是升序,true为降序
+print(test)
+print(alist)
+#https://www.cnblogs.com/wjw2018/p/10613242.html
+
+#确定字符串是否包含唯一字符,包含返回true
+s='1223'
+print(not len(set(s))==len(s))
+
+# 实现一个算法来实现反转字符数组的功能。反转的要求如下：
+# 将字符数组的字符进行反转，例如 ['b', ' ', 'a', 'r'] 变成 ['r', 'a', ' ', 'b']
